69e8c959d62239f035495d3c0914da3371c6bdc1
German Meza <iam@germanamz.com>
2026-04-16 18:41:03 -0600

    Auto-install LSP servers via mason-lspconfig

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,6 +1,22 @@
 -- LSP stack: native vim.lsp.config (Neovim 0.11+) + mason.nvim for binaries.
--- Servers are registered and enabled in a later step; this file currently only
--- bootstraps Mason so `:Mason` opens the installer UI.
+-- mason-lspconfig provides the `ensure_installed` bridge so every server in
+-- the list is auto-installed on first start. Servers are registered/enabled
+-- in a later step.
+local servers = {
+  "ts_ls",
+  "pyright",
+  "gopls",
+  "rust_analyzer",
+  "lua_ls",
+  "bashls",
+  "jsonls",
+  "yamlls",
+  "taplo",
+  "html",
+  "cssls",
+  "marksman",
+}
+
 return {
   {
     "mason-org/mason.nvim",
@@ -8,6 +24,17 @@ return {
     build = ":MasonUpdate",
     config = function()
       require("mason").setup()
+    end,
+  },
+  {
+    "mason-org/mason-lspconfig.nvim",
+    lazy = false,
+    dependencies = { "mason-org/mason.nvim" },
+    config = function()
+      require("mason-lspconfig").setup({
+        ensure_installed = servers,
+        automatic_installation = false,
+      })
     end,
   },
 }
