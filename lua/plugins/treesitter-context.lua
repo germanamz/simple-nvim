0826bb33e99051adff1d510c1cd789b078c3ceb4
German Meza <iam@germanamz.com>
2026-04-21 13:52:18 -0600

    Add nvim-treesitter-context for sticky scope headers

---
+++ lua/plugins/treesitter-context.lua
@@ -0,0 +1,15 @@
+return {
+  "nvim-treesitter/nvim-treesitter-context",
+  dependencies = { "nvim-treesitter/nvim-treesitter" },
+  event = { "BufReadPre", "BufNewFile" },
+  opts = {
+    max_lines = 4,
+    multiline_threshold = 1,
+    trim_scope = "outer",
+    mode = "cursor",
+    separator = "─",
+  },
+  keys = {
+    { "<leader>ut", function() require("treesitter-context").toggle() end, desc = "Toggle treesitter context" },
+  },
+}
